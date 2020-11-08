0f9de5e17eb961edad241aad34312c8d765da67c
Konrad <konrad@lubowicki.pl>
2020-11-08 18:13:36 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,58 @@
 import { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import "normalize.css";
 import ListItem from "./components/ListItem";
 
 function App() {
   const [query, setQuery] = useState("");
-  const [suggestions, setSugestions] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const [notFound, setNotFound] = useState(false);
   const [images, setImages] = useState([]);
   const accessKey = "5gOIvztDV9Uwc_UsC7CG3If4ZMTumuEo58gxCUtf48Q";
   const url = "https://api.unsplash.com/search/photos";
-  const url2 = "https://unsplash.com/nautocomplete/";
+  //const url2 = "https://unsplash.com/nautocomplete/";
+  //const url2 =
+  //("https://cors-anywhere.herokuapp.com/https://unsplash.com/nautocomplete/");
 
   const handleChange = (event) => {
     setQuery(event.target.value);
-    if (event.target.value === "") setImages([]);
+    if (event.target.value === "") {
+      setImages([]);
+      setNotFound(false);
+    }
+    // axios
+    //   .get(url2 + event.target.value);
+    //   .get(url2 + event.target.value, {
+    //   params: { client_id: accessKey },
+    // })
 
-    axios
-      .get(url2 + event.target.value, {
-        params: { client_id: accessKey },
-      })
-      .then((data) => {
-        setSugestions(data);
-        console.log(suggestions);
-      })
-      .catch((err) => {
-        console.log("Error happened during fetching suggestions!", err);
-      });
+    // fetch(url2 + event.target.value)
+    //   .then((data) => {
+    //     setSugestions(data);
+    //     console.log(suggestions);
+    //   })
+    //   .catch((err) => {
+    //     console.log("Error happened during fetching suggestions!", err);
+    //   });
   };
 
   const handleClick = () => {
+    setLoading(true);
     axios
       .get(url, {
-        params: { query: query, page: 1, client_id: accessKey, per_page: 24 },
+        params: { query: query, page: 1, per_page: 24 },
+        headers: {
+          Authorization: "Client-ID " + accessKey,
+        },
       })
       .then((data) => {
         setImages(data.data.results);
         console.log(images);
+        setLoading(false);
+        if (data.data.results.length === 0) {
+          setNotFound(true);
+        } else setNotFound(false);
       })
       .catch((err) => {
         console.log("Error happened during fetching!", err);
@@ -43,7 +60,7 @@ function App() {
   };
 
   const onKeyUp = (e) => {
-    if (e.charCode === 13) {
+    if (query !== "" && e.charCode === 13) {
       handleClick();
     }
   };
@@ -60,52 +77,31 @@ function App() {
     <>
       <div className="App">
         <h1>Unsplash photo search</h1>
+        {isLoading && <h1>loading</h1>}
+
         <input
+          class="searchInput"
+          name="photo"
           onChange={handleChange}
           onKeyPress={onKeyUp}
-          type="test"
-          name="photo"
           placeholder="Search for photo"
+          type="test"
         ></input>
-        <button onClick={handleClick}> Search</button>
       </div>
-      <div className="card-list">{imagesList}</div>
+      <div className="card-list">
+        {notFound && (
+          <div>
+            <h3>No images found</h3>
+            <img
+              alt="No images found"
+              src="https://unsplash.com/a/img/empty-states/photos.png"
+            />
+          </div>
+        )}
+        {imagesList}
+      </div>
     </>
   );
 }
 
 export default App;
-/* <>
-<form className="form" onSubmit={searchPhotos}>
-  {" "}
-  <label className="label" htmlFor="query">
-    {" "}
-    📷
-  </label>
-  <input
-    type="text"
-    name="query"
-    className="input"
-    placeholder={`Try "dog" or "apple"`}
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-  />
-  <button type="submit" className="button">
-    Search
-  </button>
-</form>
-
-<div className="card-list">
-  {pics.map((pic) => (
-    <div className="card" key={pic.id}>
-      <img
-        className="card--image"
-        alt={pic.alt_description}
-        src={pic.urls.full}
-        width="50%"
-        height="50%"
-      ></img>
-    </div>
-  ))}{" "}
-</div>
-</> */
